e21021e1587a55ba5cb27a0c9abf5b5321061f6d
Bruce <brucezhangpro@hotmail.com>
2020-06-02 02:37:31 -0400

    version 1.0

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 5000;
+  const PORT = 5000;
+  const port = process.env.PORT || PORT;
   app.enableCors();
   app.listen(port).then(() => {console.log(`App is listening on port ${port}`)});
 }
