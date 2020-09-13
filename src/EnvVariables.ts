9408b1db86b652c4ecbb18595a2ec2d507538f44
Bruce Zhang <brucezhangpro@hotmail.com>
2020-09-13 00:02:52 -0400

    gmail OAuth2 Config added

--- src/EnvVariables.ts
+++ src/EnvVariables.ts
@@ -5,6 +5,12 @@ const EnvVariables = {
     MAIL_PASS: process.env.MAIL_PASS,
     PASSPORT_SECRET: process.env.PASSPORT_SECRET,
     DB_URL: process.env.DB_URL,
+    GMAIL_REFRESH_TOKEN: process.env.GMAIL_REFRESH_TOKEN,
+    GMAIL_CLIENT_ID: process.env.GMAIL_CLIENT_ID,
+    GMAIL_CLIENT_SECRET: process.env.GMAIL_CLIENT_SECRET,
+    GMAIL_REDIRECT_URI: process.env.GMAIL_REDIRECT_URI,
+    GMAIL_ADDRESS: process.env.GMAIL_ADDRESS,
+    
 };
 
 export default EnvVariables;
--- src/components/auth/reset-password/reset-password.service.ts
+++ src/components/auth/reset-password/reset-password.service.ts
@@ -3,6 +3,7 @@ import clientURL from '../../../clientURL/clientURL'
 import { OrganizationService } from '../../organization/organization.service';
 import { JwtService } from '@nestjs/jwt';
 import * as nodemailer from 'nodemailer';
+import {google} from 'googleapis';
 import EnvVariables from "../../../EnvVariables";
 
 @Injectable()
@@ -13,16 +14,33 @@ export class ResetPasswordService {
     ) {}
 
     async sendMail(email: string) {
+        /*config google api OAuth*/
+        const OAuth2 = google.auth.OAuth2;
+        const oauth2Client = new OAuth2(
+          EnvVariables.GMAIL_CLIENT_ID, // ClientID
+          EnvVariables.GMAIL_CLIENT_SECRET,// Client Secret
+          EnvVariables.GMAIL_REDIRECT_URI// Redirect URL
+        );
+        oauth2Client.setCredentials({
+            refresh_token: EnvVariables.GMAIL_REFRESH_TOKEN
+        });
+        const accessToken = oauth2Client.getAccessToken()
+        /*get email address of the user*/
         const payload = await this.organizationService.getResetPasswordInfo(email);
         if(!payload) return Promise.reject('the email is not registered');
         const ResetToken = await this.jwtService.sign(payload);
         const transport = nodemailer.createTransport({
-            host: 'gmail',
+            service: "gmail",
             auth: {
                 type: "OAuth2",
-                user: "gather@noted-feat-289220.iam.gserviceaccount.com",
-                serviceClient: "100973907777663705041",
-                privateKey: "0472f595826ece117bb3deef3b21d067a12fff6d"
+                user: EnvVariables.GMAIL_ADDRESS,
+                clientId: EnvVariables.GMAIL_CLIENT_ID,
+                clientSecret: EnvVariables.GMAIL_CLIENT_SECRET,
+                refreshToken: EnvVariables.GMAIL_REFRESH_TOKEN,
+                accessToken: accessToken,
+            },
+            tls: {
+                rejectUnauthorized: false
             }
         });
         const resetLink = clientURL + '/reset/' + ResetToken + '';
